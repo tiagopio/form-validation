4b505a792e2eedc4b7149dc160ccdbec3ac69784
Tiago <97364045+tivgo@users.noreply.github.com>
2023-04-03 19:46:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ class ValidateForm {
 
    events() {
       // Listen for a "submit" event on the form and call the "handleSubmit" method.
-      this.form.addEventListener('touchend', e => {
+      this.form.addEventListener('click', e => {
          this.handleSubmit(e);
       });
    }
